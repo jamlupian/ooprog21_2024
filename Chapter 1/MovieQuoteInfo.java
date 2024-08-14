a7f33a4d78ee67ad0a1a4aff2754e97a41f60b26
jamlupian <jam.lupian@gmail.com>
2024-08-14 10:14:20 -0700

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,14 @@
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        String quote = "And the world fell into ruin.";
+        String movie = "20th Century Boys";
+        String character = "Friend";
+        int year = 1999;
+
+        System.out.println("Favorite Movie Quote:");
+        System.out.println("Quote: " + quote);
+        System.out.println("Movie: " + movie);
+        System.out.println("Character: " + character);
+        System.out.println("Year: " + year);
+    }
+}
