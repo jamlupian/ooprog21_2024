ab0d0c4c782430701bd3564211eaaa7a29be753e
jamlupian <jam.lupian@gmail.com>
2024-08-29 00:29:58 +0800

    Add files via upload

---
+++ Chapter 1/sleepIn.java
@@ -0,0 +1,9 @@
+public boolean sleepIn(boolean weekday, boolean vacation) {
+    boolean result = false;
+    
+    if (!weekday || vacation)
+      result = true;
+      
+      return result;
+  }
+  
