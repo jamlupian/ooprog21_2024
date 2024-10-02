c44df6176638c83ee778889b90f40230d13d36ed
jamlupian <jam.lupian@gmail.com>
2024-10-02 23:36:16 +0800

    Add files via upload

---
+++ Chapter 4/MathTest.java
@@ -0,0 +1,16 @@
+public class MathTest {
+
+    public static void main(String[] a) {
+    
+         System.out.println("SQRT: " + Math.sqrt(81));
+         System.out.println("SIN: " + Math.sin(81));
+         System.out.println("COS: " + Math.cos(81));
+         System.out.println("FLOOR: " + Math.floor(81.3));
+         System.out.println("CEIL: " + Math.ceil(81.5));
+         System.out.println("ROUND: " + Math.round(81));
+         System.out.println("MAX: " + Math.max(81,64));
+         System.out.println("MIN: " + Math.min(81,64));
+         System.out.println("RANDOM: " + Math.random());
+     
+    }
+}
